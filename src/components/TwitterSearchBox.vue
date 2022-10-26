7034a4deb5033e0310acda5ddc75da570b555c0e
hiYifang <a6426320@gmail.com>
2022-10-26 16:38:48 +0800

    feat: 串接 API，實現 TwitterSearchBox

--- src/components/TwitterSearchBox.vue
+++ src/components/TwitterSearchBox.vue
@@ -4,13 +4,27 @@
       class="form-control mb-3"
       type="text"
       placeholder="Search Keywords"
+      v-model.trim="keywords"
+      @keyup.enter="search"
     />
-    <button type="button" class="btn btn-dark w-100">Search</button>
+    <button type="button" class="btn btn-dark w-100" @click="searchKeyword">
+      Search
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TwitterSearchBox',
+  data() {
+    return {
+      keywords: '台灣',
+    };
+  },
+  methods: {
+    searchKeyword() {
+      this.$store.dispatch('getSearchRecentData', this.keywords);
+    },
+  },
 };
 </script>
