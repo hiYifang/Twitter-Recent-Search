c393c2fe33771da62cfcdedeba4544fdc66e15e6
hiYifang <a6426320@gmail.com>
2022-10-27 17:39:07 +0800

    feat: 串接 API，實現 TweetCard

--- src/components/TweetCard.vue
+++ src/components/TweetCard.vue
@@ -1,29 +1,29 @@
 <template>
   <li
     class="card mb-3 p-3"
-    v-for="(item, index) in cardData"
-    :key="index"
+    v-for="item in cardData"
+    :key="item.author_id"
   >
     <div class="row g-0">
-      <div class="col-5 col-sm-4 text-center">
-        <a href="#" class="stretched-link">
+      <div class="col-5 col-sm-4">
+        <a href="#" class="stretched-link" @click.prevent="open(item.id)">
           <img
-            src="https://hexschool.github.io/boootstrap5WebLayout/assets/news01.jpg"
-            class="h-100 rounded"
-            style="max-width: 100%"
-            alt="news01"
+            :src="item.profile_image_url"
+            class="position-absolute bottom-50 start-15 rounded"
+            :alt="item.username"
           />
         </a>
       </div>
       <div class="col-7 col-sm-8">
         <div class="card-body">
-          <h2 class="card-title h5 m-0">User Name</h2>
+          <h2 class="card-title h5 m-0">{{ item.username }}</h2>
           <p class="card-text text-gray my-3">
             {{ item.text }}
           </p>
           <p class="card-text m-0">
-            <small class="d-block text-muted">2022/05/12 20:15 </small>
-            <small class="d-block text-muted">23 min ago</small>
+            <small class="d-block text-muted">
+              {{ item.created_at.replace(/T/g,' ').replace(/\.[\d]{3}Z/,'') }}
+            </small>
           </p>
         </div>
       </div>
@@ -51,6 +51,12 @@ export default {
     return {
       cardData: {},
     };
+  },
+  methods: {
+    // 另開新視窗
+    open(id) {
+      window.open(`https://twitter.com/TwitterDev/status/${id}`, '_blank');
+    },
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ export default createStore({
     // state, status/payload
     GET_SEARCH_RECENT_DATA(state, payload) {
       if (payload) {
-        state.searchRecentData = payload.data;
+        state.searchRecentData = payload;
       }
     },
   },
@@ -18,13 +18,52 @@ export default createStore({
     // 非同步行為
     // context, status/payload
     getSearchRecentData(context, payload) {
-      axios.get('/api/tweets/search/recent', {
-        params: { query: payload },
-        headers: { Authorization: `Bearer ${process.env.VUE_APP_TOKEN}` },
-      }).then((res) => {
-        context.commit('GET_SEARCH_RECENT_DATA', res.data);
-      }).catch((err) => {
-        console.log(err);
+      const promise = new Promise((resolve) => {
+        // (1) 搜尋文章
+        axios.get('/api/tweets/search/recent', {
+          params: { query: payload },
+          headers: { Authorization: `Bearer ${process.env.VUE_APP_TOKEN}` },
+        }).then((resSearch) => {
+          resolve(resSearch.data.data);
+        }).catch((err) => {
+          console.log(err);
+        });
+      });
+      promise.then(async (resData) => {
+        const allData = [];
+        const promises = [];
+        for (let index = 0; index < resData.length; index += 1) {
+          promises.push(
+            // (2) 單一文章
+            axios.get(`/api/tweets/${resData[index].id}?tweet.fields=author_id,created_at,id,text`, {
+              headers: { Authorization: `Bearer ${process.env.VUE_APP_TOKEN}` },
+            }).then((resTweet) => {
+              resData[index].author_id = resTweet.data.data.author_id;
+              resData[index].created_at = resTweet.data.data.created_at;
+              allData.push(resData[index]);
+            }).catch((error) => {
+              console.log(error);
+            }),
+          );
+        }
+        await Promise.all(promises).then(() => console.log(allData));
+        return allData;
+      }).then(async (allData) => {
+        const promises = [];
+        for (let index = 0; index < allData.length; index += 1) {
+          promises.push(
+            // (3) 作者身份
+            axios.get(`/api/users/${allData[index].author_id}?user.fields=profile_image_url`, {
+              headers: { Authorization: `Bearer ${process.env.VUE_APP_TOKEN}` },
+            }).then((resAuthor) => {
+              allData[index].username = resAuthor.data.data.username;
+              allData[index].profile_image_url = resAuthor.data.data.profile_image_url;
+            }).catch((error) => {
+              console.log(error);
+            }),
+          );
+        }
+        await Promise.all(promises).then(() => context.commit('GET_SEARCH_RECENT_DATA', allData));
       });
     },
   },
